e22b644cd2c8f902840dafa9aeb5649e21d50e80
bmh201708 <49069997+bmh201708@users.noreply.github.com>
2025-11-19 19:50:09 +0800

    参数传递

--- pages/calculator/calculator.js
+++ pages/calculator/calculator.js
@@ -5,9 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    result : "",
   },
 
+  addNum : function(e){
+    this.setData ({
+      result: this.data.result + e.currentTarget.dataset.num,
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
